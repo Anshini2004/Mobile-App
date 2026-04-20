34b3cd9db702ff48ff6d3d932970515fd3b8ced4
BhaveshBundhoo <bhavesh.bundhoo@umail.uom.ac.mu>
2026-04-20 17:06:11 +0400

    fix

--- Web-assignment/chatbot/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('message/', views.chat_message, name='chat_message'),
-    path('history/', views.chat_history, name='chat_history'),
-]
